a9482ab2ff0b10c0cfea26f2007725df870d0e80
HiddenSeat <faishalnursetiawan@gmail.com>
2023-08-24 16:04:36 +0700

    doc sub add

--- src/app/features/document-submission/interfaces/document-submission.ts
+++ src/app/features/document-submission/interfaces/document-submission.ts
@@ -1,24 +1,23 @@
 export interface DocumentSubmission {
-  id: number;
-  type: number;
-  user: any;
-  user_id: number;
-  status: number;
-  stage: number;
-  created_at: string;
-  updated_at: string;
-  status_name: string;
-  type_name: string;
-  stage_name: string;
-  document_attachments: DocumentAttachment[];
-  document_progresses: any[];
+  id: number
+  type: number
+  user_id: number
+  status: number
+  stage: number
+  created_at: string
+  updated_at: string
+  status_name: string
+  type_name: string
+  stage_name: string
+  document_attachments: DocumentAttachment[]
+  document_progresses: any[]
 }
 
 export interface DocumentAttachment {
-  id: number;
-  document_submission_id: number;
-  document_type: string;
-  file_path: string;
-  created_at: string;
-  updated_at: string;
+  id: number
+  document_submission_id: number
+  document_type: string
+  file_path: string
+  created_at: string
+  updated_at: string
 }
--- src/app/features/document-submission/pages/document-submission-add/document-submission-add.component.ts
+++ src/app/features/document-submission/pages/document-submission-add/document-submission-add.component.ts
@@ -1,29 +1,32 @@
+import { Location } from '@angular/common';
 import { AfterContentInit, Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DocumentSubmissionService } from '@features/document-submission/services/document-submission.service';
 import {
   faChevronDown,
   faEye,
+  faFile,
   faPlus,
   faSave,
   faTimes,
   faTruckMoving,
 } from '@fortawesome/free-solid-svg-icons';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { LayoutService } from 'src/app/layout/services/layout.service';
-import { DocumentSubmissionService } from '@features/document-submission/services/document-submission.service';
-import { DialogService } from 'primeng/dynamicdialog';
 import { FcConfirmService } from '@shared/components/fc-confirm/fc-confirm.service';
 import { FcToastService } from '@shared/components/fc-toast/fc-toast.service';
-import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
-
+import { DialogService } from 'primeng/dynamicdialog';
+import { Subject } from 'rxjs';
+import { LayoutService } from 'src/app/layout/services/layout.service';
 
 @Component({
   selector: 'app-document-submission-add',
   templateUrl: './document-submission-add.component.html',
-  styleUrls: ['./document-submission-add.component.css']
+  styleUrls: ['./document-submission-add.component.css'],
 })
-export class DocumentSubmissionAddComponent implements OnInit, OnDestroy, AfterContentInit {
+export class DocumentSubmissionAddComponent
+  implements OnInit, OnDestroy, AfterContentInit
+{
   private destroy$: any = new Subject();
   // Icons
   faEye = faEye;
@@ -31,6 +34,7 @@ export class DocumentSubmissionAddComponent implements OnInit, OnDestroy, AfterC
   faPlus = faPlus;
   faTimes = faTimes;
   faChevronDown = faChevronDown;
+  faFile = faFile;
 
   loading = false;
   documentSubmissionForm: FormGroup;
@@ -44,26 +48,75 @@ export class DocumentSubmissionAddComponent implements OnInit, OnDestroy, AfterC
       },
     },
   ];
+  document_file_enums = [
+    'Surat Pengantar RT RW (dengan keterangan tempat usaha)',
+    'EKTP Pemilik Usaha',
+    'Foto tempat usaha',
+    'Surat Pengantar RT RW (dengan keterangan akan Menikah)',
+    'EKTP',
+    'KK',
+    'Akte Kelahiran',
+    'Ijazah terakhir',
+    'Foto 3x4 background Merah',
+    'Surat Kematian/akte cerai (janda/duda)→(jika ada)',
+    'Surat Pengantar RT RW setempat',
+    'KTP dan KK Almarhum/Almarhumah',
+    'Buku Nikah / Akte Pernikahan Almarhum / Almarhumah',
+    'Surat Keterangan kematian dari rumah sakit',
+    'Surat pemakaman dari TPU tempat dimakamkan',
+    'Akte Kematian',
+    'KTP dan KK Ahli Waris',
+    'Akte Kelahiran semua Ahli Waris',
+    'KTP saksi 2 orang',
+  ];
 
   document_type = [
     {
       id: 0,
-      name: 'Surat Keterangan Usaha'
+      name: 'Surat Keterangan Usaha',
+      required_files: [
+        'Surat Pengantar RT RW (dengan keterangan tempat usaha)',
+        'EKTP Pemilik Usaha',
+        'Foto tempat usaha',
+      ],
     },
     {
       id: 1,
-      name: 'Surat Pengantar Nikah'
+      name: 'Surat Pengantar Nikah',
+      required_files: [
+        'Surat Pengantar RT RW (dengan keterangan akan Menikah)',
+        'EKTP',
+        'KK',
+        'Akte Kelahiran',
+        'Ijazah terakhir',
+        'Foto 3x4 background Merah',
+        'Surat Kematian/akte cerai (janda/duda)→(jika ada)',
+      ],
     },
     {
       id: 2,
-      name: 'Surat Keterangan Kematian'
+      name: 'Surat Keterangan Kematian',
+      required_files: [
+        'KTP dan KK Almarhum/Almarhumah',
+        'Buku Nikah / Akte Pernikahan Almarhum / Almarhumah',
+        'Surat Keterangan kematian dari rumah sakit',
+        'Surat pemakaman dari TPU tempat dimakamkan',
+      ],
     },
     {
       id: 3,
-      name: 'Surat Ahli Waris'
-    }
-  ]
-
+      name: 'Surat Ahli Waris',
+      required_files: [
+        'Akte Kematian',
+        'KTP dan KK Almarhum/Almarhumah',
+        'Buku Nikah / Akte Pernikahan Almarhum / Almarhumah',
+        'KTP dan KK Ahli Waris',
+        'Akte Kelahiran semua Ahli Waris',
+        'KTP saksi 2 orang',
+      ],
+    },
+  ];
+  selectedDocumentType: any = null;
 
   constructor(
     private layoutService: LayoutService,
@@ -72,7 +125,8 @@ export class DocumentSubmissionAddComponent implements OnInit, OnDestroy, AfterC
     private fcConfirmService: FcConfirmService,
     private fcToastService: FcToastService,
     private router: Router,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private location: Location
   ) {
     this.layoutService.setHeaderConfig({
       title: 'Pengajuan Dokumen',
@@ -82,7 +136,7 @@ export class DocumentSubmissionAddComponent implements OnInit, OnDestroy, AfterC
     // init form
     this.documentSubmissionForm = new FormGroup({
       type: new FormControl(null, Validators.required),
-      document_attachments: new FormArray([])
+      document_attachments: new FormArray([]),
     });
   }
 
@@ -94,10 +148,91 @@ export class DocumentSubmissionAddComponent implements OnInit, OnDestroy, AfterC
     this.destroy$.complete();
   }
 
-  removeType(){
-
+  submit() {
+    let fd = new FormData();
+    fd.append('type', this.documentSubmissionForm.value.type.id);
+    if (
+      this.documentAttachments.value.length ===
+      this.documentSubmissionForm.value.type.required_files.length
+    ) {
+      this.documentSubmissionForm.value.document_attachments.forEach(
+        (file: any, i: number) => {
+          fd.append(`document_attachments[${i}][file]`, file.file);
+          fd.append(`document_attachments[${i}][document_type]`, file.type);
+        }
+      );
+      this.documentSubmissionService.createSubmission(fd).subscribe({
+        next: (res: any) => {
+          this.fcToastService.add({
+            severity: 'success',
+            header: 'Berhasil',
+            message: 'Dokumen berhasil diajukan',
+          });
+          this.back();
+        },
+        error: (err: any) => {
+          this.fcToastService.add({
+            severity: 'error',
+            header: 'Gagal',
+            message: 'Dokumen gagal diajukan',
+          });
+        },
+      });
+    } else {
+      this.fcToastService.add({
+        severity: 'warning',
+        header: 'Gagal',
+        message: 'Mohon lengkapi semua dokumen yang dibutuhkan',
+      });
+    }
+  }
+  onSelectType(event: any) {
+    this.documentSubmissionForm.removeControl('document_attachments');
+    this.documentSubmissionForm.addControl(
+      'document_attachments',
+      new FormArray([])
+    );
+  }
+  addImage(image: any, type: any) {
+    // check if has type in form
+    let fileIndex = this.documentAttachments.value.findIndex(
+      (file: any) => file.type === type
+    );
+    if (fileIndex > -1) {
+      // update image
+      this.documentAttachments.controls[fileIndex].patchValue({
+        file: image.file,
+        img_src: image.img_src,
+      });
+    } else {
+      // add image
+      this.documentAttachments.push(
+        new FormGroup({
+          file: new FormControl(image.file),
+          img_src: new FormControl(image.img_src),
+          type: new FormControl(type),
+        })
+      );
+    }
+  }
+  get documentAttachments(): FormArray {
+    return this.documentSubmissionForm.get('document_attachments') as FormArray;
+  }
+  getDocumentAttachmentFile(type: any) {
+    let file = this.documentAttachments.value.find(
+      (file: any) => file.type === type
+    );
+    if (file) {
+      return file;
+    } else {
+      return null;
+    }
   }
 
-  submit(){}
-
+  getFileName(file: any): string {
+    return file.file.name;
+  }
+  back() {
+    this.location.back();
+  }
 }

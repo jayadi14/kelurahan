86cb90e84b7bcfed6e3b7e974caaa8cc6815c4f5
HiddenSeat <faishalnursetiawan@gmail.com>
2023-08-24 15:49:36 +0700

    .

--- src/app/features/document-submission/document-submission.module.ts
+++ src/app/features/document-submission/document-submission.module.ts
@@ -1,24 +1,24 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { FcDatepickerModule } from '@shared/components/fc-datepicker/fc-datepicker.module';
+import { FcFileInputModule } from '@shared/components/fc-file-input/fc-file-input.module';
+import { FcInputTextModule } from '@shared/components/fc-input-text/fc-input-text.module';
+import { FcDropdownModule } from 'src/app/shared/components/fc-dropdown/fc-dropdown.module';
+import { FcPaginationModule } from 'src/app/shared/components/fc-pagination/fc-pagination.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { DocumentSubmissionRoutingModule } from './document-submission-routing.module';
 import { DocumentSubmissionComponent } from './document-submission.component';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { FcPaginationModule } from 'src/app/shared/components/fc-pagination/fc-pagination.module';
-import { FcDropdownModule } from 'src/app/shared/components/fc-dropdown/fc-dropdown.module';
-import { FcInputTextModule } from '@shared/components/fc-input-text/fc-input-text.module';
-import { FcDatepickerModule } from '@shared/components/fc-datepicker/fc-datepicker.module';
+import { DocumentSubmissionAddComponent } from './pages/document-submission-add/document-submission-add.component';
 import { DocumentSubmissionListComponent } from './pages/document-submission-list/document-submission-list.component';
 import { DocumentSubmissionViewComponent } from './pages/document-submission-view/document-submission-view.component';
-import { DocumentSubmissionAddComponent } from './pages/document-submission-add/document-submission-add.component';
-
 
 @NgModule({
   declarations: [
     DocumentSubmissionComponent,
     DocumentSubmissionListComponent,
     DocumentSubmissionViewComponent,
-    DocumentSubmissionAddComponent
+    DocumentSubmissionAddComponent,
   ],
   imports: [
     CommonModule,
@@ -28,6 +28,7 @@ import { DocumentSubmissionAddComponent } from './pages/document-submission-add/
     FcInputTextModule,
     FcDropdownModule,
     FcDatepickerModule,
-  ]
+    FcFileInputModule,
+  ],
 })
-export class DocumentSubmissionModule { }
+export class DocumentSubmissionModule {}
